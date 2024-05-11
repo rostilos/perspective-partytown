d2f3e95c34f9c6a4acf359eef80493fc50dc4976
rostislav <rostilosfl@gmail.com>
2024-05-11 03:45:49 +0300

    nodejs reverse-proxy sample ( minor fixes )

--- samples/nodejs-reverse-proxy/server.js
+++ samples/nodejs-reverse-proxy/server.js
@@ -6,9 +6,11 @@ import 'dotenv/config';
 const app = express();
 const myLimit = typeof (process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
 console.log('Using limit: ', myLimit);
-console.log(process.env.PORT);
 app.use(bodyParser.json({limit: myLimit}));
+
 app.all('/proxy', function (req, res, next) {
+    // Some resources do not give the correct header,
+    // because of which the script outside the tag will not load as a consequence of CORS
     res.header("Access-Control-Allow-Origin", req?.origin ?? "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
 
